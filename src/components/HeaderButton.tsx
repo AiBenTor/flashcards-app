8f8f7658db1cfcb9d8d01561155031b97403c2f8
AitorMelero <meleropiconaitor@gmail.com>
2024-05-20 13:36:20 +0200

    refactor: Change from useColor hook to COLOR constant in consts.ts

--- src/components/HeaderButton.tsx
+++ src/components/HeaderButton.tsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom'
 import { Button, styled } from '@mui/material'
-import { useColor } from '@/hooks/useColor'
+import { COLOR } from '@/utils/consts'
 
 interface Props {
   text: string
@@ -8,11 +8,10 @@ interface Props {
 }
 
 export const HeaderButton: React.FC<Props> = ({ text, url }) => {
-  const { color } = useColor()
   const CustomButton = styled(Button)({
-    color: color.black,
+    color: COLOR.BLACK,
     '&:hover': {
-      backgroundColor: color.blueDark,
+      backgroundColor: COLOR.BLUE_DARK,
     },
   })
 
---
+++ src/utils/consts.ts
@@ -0,0 +1,10 @@
+import palette from '@/styles/palette.module.scss'
+
+export const COLOR = {
+  BASE_LIGHT: palette.baseLight,
+  BLACK: palette.black,
+  BLUE_DARK: palette.blueDark,
+  BLUE_LIGHT: palette.blueLight,
+  GRAY_DARK: palette.grayDark,
+  GRAY_LIGHT: palette.grayLight,
+}
