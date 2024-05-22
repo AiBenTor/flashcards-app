50f93e2b36495d7f9cdeef03abd7c0390d875a72
AitorMelero <meleropiconaitor@gmail.com>
2024-05-22 08:39:05 +0200

    chore: Fix problem when calling window.matches() in useTheme

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,8 +2,10 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
+    globals: true,
     watch: false,
     environment: 'jsdom',
-    alias: [ { find: '@', replacement: '/src' } ]
-  }
+    alias: { '@/': new URL('./src/', import.meta.url).pathname },
+    setupFiles: 'tests/setup/setup.ts',
+  },
 })
