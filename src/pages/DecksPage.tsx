fb9e8928f05b51dee4816fa4fa4fd41efca671b3
AitorMelero <meleropiconaitor@gmail.com>
2024-05-23 12:20:32 +0200

    feat: Add add deck button, not functional

--- src/pages/DecksPage.tsx
+++ src/pages/DecksPage.tsx
@@ -1,13 +1,32 @@
 import { Deck } from '@/components/Deck'
 import { getAllDecks } from '@/utils/decks'
 import '@/styles/decksPage.scss'
+import { Fab } from '@mui/material'
+import { Add } from '@mui/icons-material'
+import { useState } from 'react'
+import { Deck as DeckType } from '@/types/deck'
 
 export const DecksPage = () => {
+  const [ decks, setDecks ] = useState(getAllDecks())
+
+  const addDeck = () => {
+    const newDeck: DeckType = {
+      id: crypto.randomUUID(),
+      title: 'New Deck',
+      description: 'English Deck Created',
+      newCards: 2,
+      learningCards: 3,
+      scheduledCards: 2,
+    }
+
+    setDecks([ ...decks, newDeck ])
+  }
+
   return (
     <>
       <h1>DecksPage</h1>
-      <div className='decks-container'>
-        {getAllDecks().map((deck) => (
+      <div className="decks-container">
+        {decks.map((deck) => (
           <Deck
             key={deck.id}
             id={deck.id}
@@ -19,6 +38,9 @@ export const DecksPage = () => {
           />
         ))}
       </div>
+      <Fab color="primary" aria-label="add" sx={{ position: 'fixed', right: '7%', bottom: '10%' }} onClick={addDeck}>
+        <Add />
+      </Fab>
     </>
   )
 }
